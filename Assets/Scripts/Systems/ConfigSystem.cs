656cecfe61a3dcb2e3d927b9d00316716d359fb6
4rs69 <4rseniyFedorov@gmail.com>
2023-03-23 18:16:45 +0300

    refactor: separated the logic of writing the model

--- Assets/Scripts/Systems/ConfigSystem.cs
+++ Assets/Scripts/Systems/ConfigSystem.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 namespace Systems
 {
-    //Система Конфига: определение формул для расчета и цены повышения уровня бизнеса на основе конфига. Загрузка файлов конфига,
-    //их хранение и предоставление доступа для других систем.
     public class ConfigSystem : MonoBehaviour
     {
         [SerializeField] private BusinessConfig _businessConfig;
@@ -33,21 +31,28 @@ namespace Systems
                 var level = _businessConfig.BusinessModels[i].Level;
                 var income = _businessConfig.BusinessModels[i].Income;
                 var price = _businessConfig.BusinessModels[i].Price;
-
-                var businessImproves = new BusinessImprovementModel[_businessConfig.BusinessModels[i].TypesImprovement.Length];
-                for (var j = 0; j < businessImproves.Length; j++)
-                {
-                    var businessImproveName = _businessConfig.BusinessModels[i].TypesImprovement[j].Name;
-                    var businessImprovePrice = _businessConfig.BusinessModels[i].TypesImprovement[j].Price;
-                    var businessImproveBoostIncome = _businessConfig.BusinessModels[i].TypesImprovement[j].BoostIncome;
-                    var businessImproveIsPurchased = _businessConfig.BusinessModels[i].TypesImprovement[j].IsPurchased;
-
-                    businessImproves[j] = new BusinessImprovementModel(businessImproveName, businessImprovePrice,
-                        businessImproveBoostIncome, businessImproveIsPurchased);
-                }
+                
+                var businessImproves = CreateBusinessImproves(i);
 
                 _businessModels[i] = new BusinessModel(businessName, incomeDelay, level, income, price, businessImproves);
             }
+        }
+
+        private BusinessImprovementModel[] CreateBusinessImproves(int i)
+        {
+            var businessImproves = new BusinessImprovementModel[_businessConfig.BusinessModels[i].TypesImprovement.Length];
+            for (var j = 0; j < businessImproves.Length; j++)
+            {
+                var businessImproveName = _businessConfig.BusinessModels[i].TypesImprovement[j].Name;
+                var businessImprovePrice = _businessConfig.BusinessModels[i].TypesImprovement[j].Price;
+                var businessImproveBoostIncome = _businessConfig.BusinessModels[i].TypesImprovement[j].BoostIncome;
+                var businessImproveIsPurchased = _businessConfig.BusinessModels[i].TypesImprovement[j].IsPurchased;
+
+                businessImproves[j] = new BusinessImprovementModel(businessImproveName, businessImprovePrice,
+                    businessImproveBoostIncome, businessImproveIsPurchased);
+            }
+
+            return businessImproves;
         }
     }
 }
